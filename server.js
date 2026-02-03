3efddde51b8cef47e0b523fe2d42af8e34337724
mruniquehacker <vaibhavkumawat1003+knightbot@gmail.com>
2026-02-03 22:49:57 +0530

    First commit

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ async function getVideoInfo(url) {
       dumpSingleJson: true,
       noWarnings: true,
       noPlaylist: true,
+      skipDownload: true, // Don't download, just get info
       addHeader: [
         'referer:youtube.com',
         'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
